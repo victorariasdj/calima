c14c1891f7997c602e6af25ef0bfa99247354d4d
victorariasdj <djvictor_arias@hotmail.com>
2023-10-20 10:34:02 -0500

    actualice la foto de promocion octubre de piscinas rs

--- codigo.js
+++ codigo.js
@@ -121,7 +121,7 @@ const negocios = [
 
     {
         nombre: "Piscinas y Accesorios RS SAS Darien y Cerrito",
-        imagen: "./img/piscinasrs.jpeg",
+        imagen: "./img/piscinabrujas.png",
         descripcion: "Suministros de insumos químicos y accesorios para un óptimo funcionamiento de su piscina, asesoría, mantenimiento, construcción, reparación de piscinas, turcos, jacuzzis, fuentes y espejos de agua, redes eléctricas y redes de gas",
         ubicacion: "Calima Darien Calle 12 No. 7 - 59 - Sede Santa Elena Cerrito calle 4 # 3-66",
         telefono: "Darien 3174424874<br>Cerrito 3174412589",
