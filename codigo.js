15dc82901057708c500bbcd54e01ea63168fef57
victorariasdj <djvictor_arias@hotmail.com>
2024-07-24 19:04:56 -0500

    reemplace todas las imagenes que no son logos por enlaces fijos, borre todas los archivos que habian en el repositorio, solo quedan los logos y unas pocas imagenes vitales

--- codigo.js
+++ codigo.js
@@ -14,7 +14,7 @@ const negocios = [
     
     {
         nombre: "Bomberos Darien",
-        imagen: "./img/bomberosdarien.jpeg",
+        imagen: "https://dbaealpkqovj2n3pebsyinzo5zkuveqlpbwxqby7agvxrvjkaj4q.arweave.net/GEBALeqDqp03byBlhDcu7lVKkgt4bXgHHwGreNUqAnk",
         descripcion: "Bomberos Darién",
         ubicacion: "",
         telefono: "3154170578",
@@ -26,7 +26,7 @@ const negocios = [
 
     {
         nombre: "Victor Cargas, Mudanzas y Fletes",
-        imagen: "./img/victormudanza.jpeg",
+        imagen: "https://b7ziznnsgijsbvdql7pkxfq6aqki24zlsu6weprovszclb4q5upq.arweave.net/D_KMtbIyEyDUcF_eq5YeBBSNcyuVPWI-LqyyJYeQ7R8",
         descripcion: "Todo tipo de carga a nivel local y nacional, con ubicación satelital por si deseas saber el curso de tu carga, ARL por si deseas cargar en alguna empresa que lo requiera. Seriedad y cumplimiento en el servicio.",
         ubicacion: "Calima el Darién, Servicio 24/7 con disponibilidad en dónde la requieran.",
         telefono: "3157915586",
@@ -38,7 +38,7 @@ const negocios = [
     
     {
         nombre: "Defensa Civil",
-        imagen: "./img/defensacivil.jpg",
+        imagen: "https://zuoz4hk434cepdjvi3rivtytjlqqo3b23uwp5mu2tcpwp3vp2bea.arweave.net/zR2eHVzfBEeNNUbiis8TSuEHbDrdLP6ympifZ-6v0Eg",
         descripcion: "",
         ubicacion: "Calle 10 # 5-36",
         telefono: "3188463732",
@@ -50,7 +50,7 @@ const negocios = [
 
     {
         nombre: "Goku Burger",
-        imagen: "./img/goku-burger.jpg",
+        imagen: "https://ijb2txx6k4dll3zrcgo4woabnrn46kshemv4d4bqeqo5h4qb4jvq.arweave.net/QkOp3v5XBrXvMRGdyzgBbFvPKkcjK8HwMCQd0_IB4ms",
         descripcion: "Abierto jueves a lunes, de 3:30 pm a 10:30 pm. Fritanga y comidas rápidas. picadas, hamburguesas, salchipapas, jugos naturales, Patacones, todo tipo de arepas.</b>",
         ubicacion: "calle 12 # 9 - 66 dos quebradas servicio a domicilio",
         telefono: "3233989891 - 3184584656",
@@ -62,7 +62,7 @@ const negocios = [
 
     {
         nombre: "Hospital San Jorge",
-        imagen: "./img/hospitalsanjorge.jpg",
+        imagen: "https://6n5qi7wchqe5limaz7vrzd4cse7xrg6wq5qsooa2kf33sze4amuq.arweave.net/83sEfsI8CdWhgM_rHI-CkT94m9aHYSc4GlF3uWScAyk",
         descripcion: "",
         ubicacion: "Darién",
         telefono: "6022395008",
@@ -74,7 +74,7 @@ const negocios = [
 
     {
         nombre: "Inmobiliaria Calima",
-        imagen: "./img/inmobiliariacalima.png",
+        imagen: "https://sbjup5clwxwbxwqky5sommovcu2fgzcnlgvg2lmtohdybudcfira.arweave.net/kFNH9Eu17BvaCsdk5jHVFTRTZE1Zqm0tk3HHgNBiKiI",
         descripcion: "Estamos aquí para brindarte la seguridad y el apoyo jurídico e inmobiliario que usted necesita. Nuestro enfoque va más allá de las ventas; se trata de guiarte hacia la mejor decisión con confianza y transparencia.",
         ubicacion: "Calle 8 # 6-50 Calima Darién",
         telefono: "3015636844",
@@ -98,7 +98,7 @@ const negocios = [
 
     {
         nombre: "Droguería Don Victor",
-        imagen: "./drogueria-don-victor/donvictorcuadrado.jpg",
+        imagen: "https://qh4vs2hy36kfrdxrabupy5ok7fdfk5itzylerx7gq5ikb2v6jgcq.arweave.net/gflZaPjflFiO8QBo_HXK-UZVdRPOFkjf5odQoOq-SYU",
         descripcion: "Droguería y Tienda Naturista, Servicio de Baloto Pago de Facturas, Giros y Recargas, Servicio de Inyectología",
         ubicacion: "Cra 7 # 11-12",
         telefono: "3155178654 - 3173805598",
@@ -110,7 +110,7 @@ const negocios = [
 
     {
         nombre: "Lavadoras Nando",
-        imagen: "./img/lavadorasnando.jpeg",
+        imagen: "https://6y3dabvyqwo56j4ouaddnwt3eervoojo4iifrhkq55diho5wunyq.arweave.net/9jYwBriFnd8njqAGNtp7ISNXOS7iEFidUO9Gg7u2o3E",
         descripcion: "Alquiler de lavadoras, reparación, mantenimiento, repuestos, todo sobre lavadoras.",
         ubicacion: "Servicio a domicilio.",
         telefono: "3184436947 (whatsapp) - 3178004643",
@@ -122,7 +122,7 @@ const negocios = [
 
     {
         nombre: "La Casona Restaurante y Comidas Rápidas",
-        imagen: "./img/lacasonafrente.jpg",
+        imagen: "https://m4wimw7cinamdvnr4edli5e5zb67pq2thgkpj4dzblk7hgu4cjmq.arweave.net/ZyyGW-JDQMHVseEGtHSdyH33w1M5lPTweQrV85qcElk",
         descripcion: "Abrimos todos los días.<br>Ricos desayunos, almuerzos ejecutivos y almuerzos a la carta de 8 am a 4 pm.<br>Comidas rápidas 6 a 11 pm.<br>Variedad de platos, con la mejor atención.",
         ubicacion: "Parque Los fundadores, enseguida de la Chaverona 2do piso. Servicio a Domicilio.",
         telefono: "3126933829",
@@ -146,7 +146,7 @@ const negocios = [
 
     {
         nombre: "Mazamorra Maxi",
-        imagen: "./img/mazamorramaxi.jpg",
+        imagen: "https://wr235sp2cj3efllrqxregaoxdgkzkdqrwrbz6zofi4zihkolbh2a.arweave.net/tHW-yfoSdkKtcYXiQwHXGZWVDhG0Q59lxUcyg6nLCfQ",
         descripcion: "Mazamorra y Champús Maxi 100% natural.",
         ubicacion: "",
         telefono: "3215076117",
@@ -158,7 +158,7 @@ const negocios = [
 
     {
         nombre: "Mari's Pizza",
-        imagen: "./img/marispizza.jpg",
+        imagen: "https://utbtotaieg5donfqksoq4munrygkj4iotyz7vv2ev2vkw36rnjda.arweave.net/pMM3TAghujc0sFSdDjKNjgyk8Q6eM_rXRK6qq2_RakY",
         descripcion: "Pizza, hamburguesa, perros, sandwich, lasaña, salchipapa, empanadas, pollo a la broaster, jugos naturales, micheladas, Jueves a Martes de 5:00 pm a 10:00 pm,",
         ubicacion: "Ciudadela German Mejia, Calle Principal Frente al Coliseo. Servicio a Domicilio.",
         telefono: "3145018504",
@@ -170,7 +170,7 @@ const negocios = [
 
     {
         nombre: "Policía Cuadrante Darién",
-        imagen: "./img/policia.jpeg",
+        imagen: "https://gipw2rafa4ric3mwe6na6oie5cogffzipxbxw2kxz54c4cxcgera.arweave.net/Mh9tRAUHIoFtlieaDzkE6Jxilyh9w3tpV894LgriMSI",
         descripcion: "Policía Cuadrante Darién",
         ubicacion: "Parque Principal",
         telefono: "3172607745",
@@ -182,7 +182,7 @@ const negocios = [
 
     {
         nombre: "Tienda Naturista Vive Mejor",
-        imagen: "./img/frentetiendavivemejor.jpg",
+        imagen: "https://hemkv6k756eisjkmkq6z4lsn42bmupomnqicwrcarqmnsheyyfha.arweave.net/ORiq-V_viIklTFQ9ni5N5oLKPcxsECtEQIwY2RyYwU4",
         descripcion: "Venta de productos naturales, citrato de magnesio, ar17, flexdol, artrim, colágenos, biotinas, vitamina c, tratamientos para artritis, insomnio, colesterol, estreñimiento, dolores articulares artrosis, artritis.",
         ubicacion: "Calle 10 # 5-36 por la defensa civil",
         telefono: "3233976017",
@@ -194,7 +194,7 @@ const negocios = [
 
     {
         nombre: "TransCalima",
-        imagen: "./img/bus.jpg",
+        imagen: "https://npa4kmdpc2dbqwip6flygzne6i2bg2tpqaxjkmibj2gevjfqv2bq.arweave.net/a8HFMG8WhhhZD_FXg2Wk8jQTam-ALpUxAU6MSqSwroM",
         descripcion: "<b>DARIÉN - BUGA</b><br><br>DARIÉN - BUGA (VÍA LA CECILIA)<br>7:00 a.m. - 05:00 p.m.<br><br> DARIÉN - BUGA (VÍA LAGO CALIMA)<br>06:30 a.m. - 8:30 a.m. - 11:00 a.m.1:30 p.m. - 3:30 p.m. - 6:00 p.m.</br></br>DARIÉN - BUGA (VÍA JIGUALES)</br>5:30 a.m. - 6:00 a.m. - 7:30 a.m. - 8:00 a.m. - 9:00 a.m. - 9:30 a.m. - 10:00 a.m. - 10:30 a.m. - 11:30 a.m. - 12:00 p.m. - 12:30 p.m. - 1:00 p.m. - 2:00 p.m. - 2:30 p.m. - 3:00 p.m. - 4:00 p.m. - 4:30 p.m. - 5:30 p.m. - 6:30 p.m.<br><br><b>BUGA - DARIÉN</b><br><br>BUGA - DARIÉN (VÍA JIGUALES)<br>6:00 a.m. - 7:00 a.m. - 7:30 a.m. - 9:00 a.m. - 9:30 a.m. - 10:00 a.m. - 10:30 a.m. - 11:30 a.m. - 12:00 p.m. - 1:00 p.m. - 1:30 p.m. - 2:30 p.m. - 3:00 p.m. - 3:30 p.m. - 5:00 p.m. - 5:30 p.m. - 6:00 p.m. - 7:00 p.m.<br><br> BUGA - DARIÉN (VÍA LA CECILIA)<br>8:00 a.m. - 4:30 p.m.<br><br>BUGA - DARIÉN ( VÍA LAGO CALIMA)<br>6:30 a.m. - 8:30 a.m. - 11:00 a.m. - 12:30 p.m. - 2:00 p.m. - 4:00 p.m. -  6:30 p.m.<br><br><b>DARIÉN - CALI</b><br><br>DARIÉN - CALI (VÍA DAGUA)<br>6:00 a.m. - 8:00 a.m - 4:00 p. m.<br><br>DARIÉN CALI (VÍA JIGUALES)<br>5:00 a.m. - 7:00 a.m. - 11:00 a.m. - 03:00 p.m. - 6:00 p.m.<br><br>DARIÉN - CALI (VÍA LAGO CALIMA)<br>9:00 a.m. - 10:00 a.m. - 12:00 p.m.1:00 p.m. - 2:00 p.m. - 5:00 p.m.<br><br><b>CALI - DARIÉN</b><br><br>CALI - DARIÉN (VÍA DAGUA)<br>7:00 a.m. - 3:00 p.m. - 4:30 p.m.<br><br>CALI - DARIÉN (VÍA JIGUALES)<br>10:00 a.m. - 1:00 p.m. - 4:00 p.m. - 6:30 p.m.<br><br>CALI - DARIÉN (VÍA LAGO CALIMA)<br>6:00 a.m. - 8:00 a.m. - 9:00 a.m. - 11:00 a.m. - 12:00 p.m. - 2:00 p.m. - 5:30 p.m.",
         ubicacion: "",
         telefono: "3108494364",
@@ -206,7 +206,7 @@ const negocios = [
 
     {
         nombre: "Smaz Papelería y Miscelánea",
-        imagen: "./img/smaz-papeleria.jpg",
+        imagen: "https://iby33otgpobgru364hldhabaabgz25a2k67nmxfo7eirxjkn6k7q.arweave.net/QHG9umZ7gmjTfuHWM4AgAE2ddBpXvtZcrvkRG6VN8r8",
         descripcion: "Ven y conoce una forma diferente tierna y divertida en papelería, manejamos servicios de: fotocopias blanco y negro o a color, impresión láser, papelería general y papelería kawaii, miscelánea entre otros. Se vale antojarse!",
         ubicacion: "Carrera 11 # 10-54 B/San Vicente",
         telefono: "3187044288",
@@ -218,7 +218,7 @@ const negocios = [
 
     {
         nombre: "Victor Arias",
-        imagen: "./img/victor-arias.jpg",
+        imagen: "https://66dbmwxnykta6ae2zhb7gaaoiqxtckj5uh6mst7gazmtyonmc5aa.arweave.net/94YWWu3Cpg8AmsnD8wAORC8xKT2h_MlP5gZZPDmsF0A",
         descripcion: "Dj de música electrónica, técnico de computadores, desarrollador web, mezcla y master de audio, clases personalizadas de informática y producción musical.",
         ubicacion: "Colombia",
         telefono: "3163251628",
